4ce6b9c2a056bd428aa38c41597ea4e66766c912
Brandon Scott <brandon.scott03@gmail.com>
2013-08-15 23:38:39 +0100

    Fixed project name

--- Sharparam.SharpBlade/Razer/RazerManager.cs
+++ Sharparam.SharpBlade/Razer/RazerManager.cs
@@ -19,7 +19,7 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * 
- * Disclaimer: SwitchBlade is in no way affiliated
+ * Disclaimer: SharpBlade is in no way affiliated
  * with Razer and/or any of its employees and/or licensors.
  * Adam Hellberg does not take responsibility for any harm caused, direct
  * or indirect, to any Razer peripherals via the use of SharpBlade.
