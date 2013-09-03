89f4a71edb191628df51a19543c3d465bfb5e913
Adam Hellberg <sharparam@sharparam.com>
2013-09-04 00:27:14 +0200

    Updated project files

--- Sharparam.SharpBlade/Properties/AssemblyInfo.cs
+++ Sharparam.SharpBlade/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.125.2121")]
-[assembly: AssemblyFileVersion("1.0.125.2121")]
+[assembly: AssemblyVersion("1.0.126.0024")]
+[assembly: AssemblyFileVersion("1.0.126.0024")]
